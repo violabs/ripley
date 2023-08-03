a2cb5385ef051fc45066508fdca94303810e327b
violabs-josh <jstallnick@violabs.io>
2023-08-02 23:30:18 -0500

    Tests for Utils file

--- src/commonTest/kotlin/io/violabs/ripley/common/UtilsTest.kt
+++ src/commonTest/kotlin/io/violabs/ripley/common/UtilsTest.kt
@@ -2,117 +2,296 @@ package io.violabs.ripley.common
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class UtilsTest {
 
-    @Test
-    fun parseAsList_will_return_empty_list_if_null() {
-        val result = null.parseAsList<String>()
+    object ParseAsListTest {
+        @Test
+        fun parseAsList_will_return_empty_list_if_null() {
+            val result = null.parseAsList<String>()
 
-        assertTrue(result.isEmpty())
+            assertTrue(result.isEmpty())
+        }
+
+        @Test
+        fun parseAsList_will_return_empty_list_if_not_list() {
+            val result = "not a list".parseAsList<String>()
+
+            assertTrue(result.isEmpty())
+        }
+
+        @Test
+        fun parseAsList_will_return_a_list_containing_only_expected_types() {
+            val result = listOf("a", 1, "b", 2).parseAsList<String>()
+
+            assertTrue(result.size == 2)
+            assertTrue(result[0] == "a")
+            assertTrue(result[1] == "b")
+        }
+
     }
 
-    @Test
-    fun parseAsList_will_return_empty_list_if_not_list() {
-        val result = "not a list".parseAsList<String>()
+    object ParseAsMapTest {
+        @Test
+        fun parseAsMap_will_return_empty_map_if_null() {
+            val result = null.parseAsMap<String, String>()
 
-        assertTrue(result.isEmpty())
+            assertTrue(result.isEmpty())
+        }
+
+        @Test
+        fun parseAsMap_will_return_empty_map_if_not_map() {
+            val result = "not a map".parseAsMap<String, String>()
+
+            assertTrue(result.isEmpty())
+        }
+
+        @Test
+        fun parseAsMap_will_return_a_map_containing_only_expected_types() {
+            val result =
+                mapOf(
+                    "a" to 1,
+                    "b" to 2,
+                    "c" to "not number",
+                    4 to "not number"
+                )
+                    .parseAsMap<String, Int>()
+
+            assertTrue(result.size == 2)
+            assertTrue(result["a"] == 1)
+            assertTrue(result["b"] == 2)
+        }
+
     }
 
-    @Test
-    fun parseAsList_will_return_a_list_containing_only_expected_types() {
-        val result = listOf("a", 1, "b", 2).parseAsList<String>()
+    object ParseAsMultiValueMapTest {
 
-        assertTrue(result.size == 2)
-        assertTrue(result[0] == "a")
-        assertTrue(result[1] == "b")
+        @Test
+        fun parseAsMultiValueMap_will_return_empty_map_if_null() {
+            val result = null.parseAsMultiValueMap<String, String>()
+
+            assertTrue(result.isEmpty())
+        }
+
+        @Test
+        fun parseAsMultiValueMap_will_return_empty_map_if_not_map() {
+            val result = "not a map".parseAsMultiValueMap<String, String>()
+
+            assertTrue(result.isEmpty())
+        }
+
+        @Test
+        fun parseAsMultiValueMap_will_return_a_map_containing_only_expected_types() {
+            val result =
+                mapOf(
+                    "a" to listOf(1, 2),
+                    "b" to listOf("not number", "not number"),
+                    "c" to listOf(3, "not number"),
+                    4 to listOf("not number", "not number")
+                )
+                    .parseAsMultiValueMap<String, Int>()
+
+            assertEquals(3, result.size)
+            assertEquals(listOf(1, 2), result["a"])
+            assertEquals(listOf(), result["b"])
+            assertEquals(listOf(3), result["c"])
+        }
+
     }
 
-    @Test
-    fun parseAsMap_will_return_empty_map_if_null() {
-        val result = null.parseAsMap<String, String>()
+    object ParseAsMatrixTest {
+        @Test
+        fun parseAsMatrix_will_return_empty_matrix_if_null() {
+            val result = null.parseAsMatrix<String>()
 
-        assertTrue(result.isEmpty())
+            assertTrue(result.isEmpty())
+        }
+
+        @Test
+        fun parseAsMatrix_will_return_empty_matrix_if_not_list() {
+            val result = "not a list".parseAsMatrix<String>()
+
+            assertTrue(result.isEmpty())
+        }
+
+        @Test
+        fun parseAsMatrix_will_return_matrix_based_on_type() {
+            val result: Matrix<String> = listOf(
+                listOf("a", "b"),
+                listOf("c", 1)
+            ).parseAsMatrix<String>()
+
+            assertEquals(2, result.size)
+            assertEquals(listOf("a", "b"), result[0])
+            assertEquals(listOf("c"), result[1])
+        }
     }
 
-    @Test
-    fun parseAsMap_will_return_empty_map_if_not_map() {
-        val result = "not a map".parseAsMap<String, String>()
+    object IffElseThenTest {
+        @Test
+        fun iff_with_then_will_evaluate_true_for_or_with_any_true() {
+            var timesCalled = 1
+            var completed = false
 
-        assertTrue(result.isEmpty())
+            iff(false)
+                .or {
+                    timesCalled++
+                    true
+                }
+                .or {
+                    timesCalled++
+                    false
+                }
+                .then { completed = true }
+
+            assertEquals(2, timesCalled)
+            assertTrue(completed)
+        }
+
+        @Test
+        fun iff_with_then_will_evaluate_false_for_or_with_all_false() {
+            var timesCalled = 1
+            var completed = false
+
+            iff(false)
+                .or {
+                    timesCalled++
+                    false
+                }
+                .or {
+                    timesCalled++
+                    false
+                }
+                .then { completed = true }
+
+            assertEquals(3, timesCalled)
+            assertFalse(completed)
+        }
+
+        @Test
+        fun iff_with_then_will_evaluate_true_for_and_with_all_true() {
+            var timesCalled = 1
+            var completed = false
+
+            iff(true)
+                .and {
+                    timesCalled++
+                    true
+                }
+                .and {
+                    timesCalled++
+                    true
+                }
+                .then { completed = true }
+
+            assertEquals(3, timesCalled)
+            assertTrue(completed)
+        }
+
+        @Test
+        fun iff_with_then_will_evaluate_false_for_or_with_any_false() {
+            var timesCalled = 1
+            var completed = false
+
+            iff(false)
+                .and {
+                    timesCalled++
+                    true
+                }
+                .and {
+                    timesCalled++
+                    true
+                }
+                .then { completed = true }
+
+            assertEquals(1, timesCalled)
+            assertFalse(completed)
+        }
     }
 
-    @Test
-    fun parseAsMap_will_return_a_map_containing_only_expected_types() {
-        val result =
-            mapOf(
-                "a" to 1,
-                "b" to 2,
-                "c" to "not number",
-                4 to "not number"
-            )
-            .parseAsMap<String, Int>()
+    object IffElseReturnTest {
+        private const val t = "TRUE"
+        private const val f = "FALSE"
 
-        assertTrue(result.size == 2)
-        assertTrue(result["a"] == 1)
-        assertTrue(result["b"] == 2)
-    }
+        @Test
+        fun iff_with_then_will_return_first_for_or_with_any_true() {
+            var timesCalled = 1
 
-    @Test
-    fun parseAsMultiValueMap_will_return_empty_map_if_null() {
-        val result = null.parseAsMultiValueMap<String, String>()
+            val actual =
+                iff(false)
+                    .or {
+                        timesCalled++
+                        true
+                    }
+                    .or {
+                        timesCalled++
+                        false
+                    }
+                    .thenReturn(t, elseReturn = f)
 
-        assertTrue(result.isEmpty())
-    }
+            assertEquals(2, timesCalled)
+            assertEquals(t, actual)
+        }
 
-    @Test
-    fun parseAsMultiValueMap_will_return_empty_map_if_not_map() {
-        val result = "not a map".parseAsMultiValueMap<String, String>()
+        @Test
+        fun iff_with_then_will_return_second_for_or_with_all_false() {
+            var timesCalled = 1
 
-        assertTrue(result.isEmpty())
-    }
+            val actual =
+                iff(false)
+                    .or {
+                        timesCalled++
+                        false
+                    }
+                    .or {
+                        timesCalled++
+                        false
+                    }
+                    .thenReturn(t, elseReturn = f)
 
-    @Test
-    fun parseAsMultiValueMap_will_return_a_map_containing_only_expected_types() {
-        val result =
-            mapOf(
-                "a" to listOf(1, 2),
-                "b" to listOf("not number", "not number"),
-                "c" to listOf(3, "not number"),
-                4 to listOf("not number", "not number")
-            )
-            .parseAsMultiValueMap<String, Int>()
+            assertEquals(3, timesCalled)
+            assertEquals(f, actual)
+        }
 
-        assertEquals(3, result.size)
-        assertEquals(listOf(1, 2), result["a"])
-        assertEquals(listOf(), result["b"])
-        assertEquals(listOf(3), result["c"])
-    }
+        @Test
+        fun iff_with_then_will_return_first_for_and_with_all_true() {
+            var timesCalled = 1
 
-    @Test
-    fun parseAsMatrix_will_return_empty_matrix_if_null() {
-        val result = null.parseAsMatrix<String>()
+            val actual =
+                iff(true)
+                    .and {
+                        timesCalled++
+                        true
+                    }
+                    .and {
+                        timesCalled++
+                        true
+                    }
+                    .thenReturn(t, elseReturn = f)
 
-        assertTrue(result.isEmpty())
-    }
+            assertEquals(3, timesCalled)
+            assertEquals(t, actual)
+        }
 
-    @Test
-    fun parseAsMatrix_will_return_empty_matrix_if_not_list() {
-        val result = "not a list".parseAsMatrix<String>()
+        @Test
+        fun iff_with_then_will_return_second_for_or_with_any_false() {
+            var timesCalled = 1
 
-        assertTrue(result.isEmpty())
-    }
+            val actual = iff(false)
+                .and {
+                    timesCalled++
+                    true
+                }
+                .and {
+                    timesCalled++
+                    true
+                }
+                .thenReturn(t, elseReturn = f)
 
-    @Test
-    fun parseAsMatrix_will_return_matrix_based_on_type() {
-        val result: Matrix<String> = listOf(
-            listOf("a", "b"),
-            listOf("c", 1)
-        ).parseAsMatrix<String>()
-
-        assertEquals(2, result.size)
-        assertEquals(listOf("a", "b"), result[0])
-        assertEquals(listOf("c"), result[1])
+            assertEquals(1, timesCalled)
+            assertEquals(f, actual)
+        }
     }
 }
